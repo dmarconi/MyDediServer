e4b4f36739edc47084e9fd0335f1ec687ef4b2c0
Daniele Marconi <daniele.marconi@desy.de>
2016-10-26 19:55:12 +0200

    Added Windows support

---
+++ Tools/StartDSTServers.bat
@@ -0,0 +1,4 @@
+C:\Users\danie\OneDrive\Documents\steamcmd\steamcmd.exe +login anonymous +app_update 343050 validate +quit
+cd /D "C:\Users\danie\OneDrive\Documents\steamcmd\steamapps\common\Don't Starve Together Dedicated Server\bin"
+start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Master
+start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Caves
